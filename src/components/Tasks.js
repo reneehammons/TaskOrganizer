0ee83770ad1d4c650183e0685929226f6cb96399
Renee Hammons <reneehammons@Renees-MBP-2.lan1>
2021-07-13 23:45:38 -0400

    Added a sort function to the tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,17 +1,9 @@
 import Task from './Task'
 
 const Tasks = ({ tasks, onDelete, onHighlight }) => {
-    const sortedTasks = tasks.sort((a,b) => {
-        const aDateAndTime = a.date + ' ' + a.time
-        const bDateAndTime = b.date + ' ' + b.time
-
-        return bDateAndTime - aDateAndTime
-    })
-    
-
     return (
         <div className="tasks">
-            {sortedTasks.map((task, index) => (
+            {tasks.map((task, index) => (
                 <Task 
                     key={index} 
                     task={task} 
